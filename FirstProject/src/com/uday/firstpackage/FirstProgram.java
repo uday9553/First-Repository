093221f304342f0aaf3321b4bc5db8279c554677
uday <uday9553@outlook.com>
2019-10-21 22:25:18 +0530

    Added second file

--- FirstProject/src/com/uday/firstpackage/FirstProgram.java
+++ FirstProject/src/com/uday/firstpackage/FirstProgram.java
@@ -3,7 +3,8 @@ package com.uday.firstpackage;
 public class FirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World ");
+		System.out.println("project updated");
 	}
 
 }
---
+++ FirstProject/src/com/uday/firstpackage/SecondProgram.java
@@ -0,0 +1,6 @@
+package com.uday.firstpackage;
+
+public class SecondProgram {
+	
+
+}
